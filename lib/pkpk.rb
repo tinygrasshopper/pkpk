568425b369d7235de918f2bf6c3055bfddaf368c
Jatin Naik <jsnaik@gmail.com>
2013-10-26 15:06:12 +0530

    Handeling nil's

--- lib/pkpk.rb
+++ lib/pkpk.rb
@@ -1,5 +1,11 @@
 module Kernel #oh, yeah!
-  def pkpk(payload)
-    p payload
+  def pkpk(*payloads)
+    if payloads.size == 0
+      pkpk nil
+    else
+      payloads.each do |payload|
+        p payload
+      end
+    end  
   end
 end
